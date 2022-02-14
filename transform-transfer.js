048379ddeebdaea6c621dc48e4a0875bcd14a28a
Ismael Agchar <ismael.agchar@gmail.com>
2022-02-14 20:18:03 +0100

    refactored token logging

--- transform-transfer.js
+++ transform-transfer.js
@@ -16,26 +16,32 @@ function transform(tokens) {
 }
 
 import { sleep } from './util.js'
+import { Rest, Tone } from './tokenize.js'
 
 async function transfer(tokens, destination) {
   // send tones to midi input device 'destination'
 
   for (const token of tokens) {
+    const now = Math.round(performance.now())
+
     // handle rest
-    if (token.type === 'rest') {
-      console.log(`${Math.round(performance.now())} R   ${Math.round(token.duration)}`)
+    if (token instanceof Rest) {
+      console.log(`${now} R   ${Math.round(token.duration)}`)
       await sleep(token.duration)
     }
     // handle tone
-    else {
+    else if (token instanceof Tone) {
       const noteOn = [0x90, token.noteNumber, token.velocity]
       const noteOff = [0x80, token.noteNumber, 0]
   
-      console.log(`${Math.round(performance.now())} ${token.pitch} ${Math.round(token.duration)}`)
+      console.log(`${now} ${token.pitch} ${Math.round(token.duration)} ${token.velocity}`)
 
       destination.send(noteOn)
       await sleep(token.duration)
       destination.send(noteOff)
+    }
+    else {
+      console.log(`${now} unkown token`)
     }
   }
 }
