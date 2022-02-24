fbf21777cf62befb188bde8eca38dece53be84cb
Ismael Agchar <ismael.agchar@gmail.com>
2022-02-24 23:03:21 +0100

    refactored token pheno typer
    - renamed to lexer
    - minor fixes
    - added sample lexing rules

---
+++ parsing/lex.mjs
@@ -0,0 +1,9 @@
+export default {
+
+  a: o => o === 'a' ,
+
+  b: o => o === 'b' ,
+
+  lol: o => o === 'lol'
+
+}
---
+++ parsing/lexer.mjs
@@ -0,0 +1,55 @@
+export class Token {
+  constructor(name, value) {
+    this.name = name
+    this.value = value
+  }
+}
+
+export class Lexer {
+
+  /**
+  * @constructor
+  * @param {Dict<string, predicate>} rules
+  * @returns {Lexer}
+  */
+  constructor(rules) {
+    this.rules = rules
+  }
+
+  /**
+   * @returns {Array<String>}
+   */
+  terminals() {
+    return Object.keys(this.rules)
+  }
+
+  /**
+   * @param {Object} obj 
+   * @param {Number} index 
+   * @returns {Token}
+   */
+  lex(obj, index) {
+    let tokenName
+    for (const name in this.rules) {
+      const predicate = this.rules[name]
+      if (predicate(obj)) {
+        tokenName = name
+        break
+      }
+    }
+
+    if (tokenName === undefined)
+      throw new Error(`no fitting phenotype rule for token ${index}`)
+
+    return new Token(tokenName, obj)
+  }
+
+  /**
+   * @param {...any} objs 
+   * @returns {Array<Token>}
+   */
+  run(...objs) {
+    return objs.map(this.lex, this)
+  }
+
+}
